6982f7b7ce2a66c856b769652a2f54ac0bce1ffc
reallyhategithub <castleracer@outlook.com>
2022-04-03 11:07:10 -0500

    fixed

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 (function() {
     const buttonLeft = document.querySelector('[data-button-left]')
     const buttonRight = document.querySelector('[data-button-right]')
-    let cardContainer = document.querySelector('[card-container]')
-
-    // let prevPic = currentPicture.previousElementSibling;
-    let nextPic;
 
     buttonLeft.addEventListener('click', function(){
         let currentCard = document.querySelector('.card.view')
@@ -25,7 +21,6 @@
         currentCard.classList.remove('view'); 
         nextCard.classList.add('view');
         dotDetectsSlide();
-        // also advance the dot
         
     }
 
@@ -33,36 +28,58 @@
 
     function dotDetectsSlide() {
         const dotContainer = document.querySelector('[data-dot-container]').childNodes;
-        // const currentCard = document.querySelector('.card.view');
         const cardOne = document.querySelector('#card-one');
         const cardTwo = document.querySelector('#card-two');
         const cardThree = document.querySelector('#card-three');
         const dotOne = document.querySelector('#dot-one');
         const dotTwo = document.querySelector('#dot-two');
         const dotThree = document.querySelector('#dot-three');
-        console.log(dotContainer.item(1).value)
 
         cardOne.classList.contains('view') ? dotOne.checked = true : false;
         cardTwo.classList.contains('view') ? dotTwo.checked = true : false;
         cardThree.classList.contains('view') ? dotThree.checked = true : false;
-
-
-        // if(currentCard == cardContainer.item(1)){
-        //     console.log('dot 1 should be blue')
-        //     allDots.item(2).classList.add('dot-filled');
-
-        // }
-
     }
 
+    function slideDetectsDot() {
+        const cardOne = document.querySelector('#card-one');
+        const cardTwo = document.querySelector('#card-two');
+        const cardThree = document.querySelector('#card-three');
+        const dotOne = document.querySelector('#dot-one');
+        const dotTwo = document.querySelector('#dot-two');
+        const dotThree = document.querySelector('#dot-three');
+
+        if(dotOne.checked) {
+            cardTwo.classList.contains('view') ? cardTwo.classList.remove('view') : false;
+            cardThree.classList.contains('view') ? cardThree.classList.remove('view') : false;
+            cardOne.classList.add('view');
+        }
 
 
+        if(dotTwo.checked) {
+            cardOne.classList.contains('view') ? cardOne.classList.remove('view') : false;
+            cardThree.classList.contains('view') ? cardThree.classList.remove('view') : false;
+            cardTwo.classList.add('view');
+        }
+
+
+        if(dotThree.checked) {
+            cardOne.classList.contains('view') ? cardOne.classList.remove('view') : false;
+            cardTwo.classList.contains('view') ? cardTwo.classList.remove('view') : false;
+            cardThree.classList.add('view');
+        }
+
+        dotOne.checked ? cardOne.classList.add('view') : false;
+        cardTwo.classList.contains('view') ? dotTwo.checked = true : false;
+        cardThree.classList.contains('view') ? dotThree.checked = true : false;
+ 
+    }
+
+    const daDots = document.querySelectorAll('[data-dot-container]');
+    daDots.forEach(child => {
+        child.addEventListener('click', slideDetectsDot);
+    });
 
     //Uncomment line below to automate
     let automateSlides = setInterval(nextSlide, 3600);
-
-    //PUSH THIS LOCAL REPO TO GITHUB ASAP!
-
 }
-
 )()    
